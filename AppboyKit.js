a5b182fc9d8bf55d8eebebcc94a0c61b2ce283ae
Robert Ing <ring@mparticle.com>
2018-08-28 13:30:41 -0400

    Load appboy via npm

--- AppboyKit.js
+++ AppboyKit.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
+var appboy = require('appboy-web-sdk');
+window.appboy = appboy;
 //  Copyright 2015 mParticle, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -222,39 +224,14 @@
                 }
 
                 if (testMode !== true) {
-                    /* eslint-disable */
-                    +function() {
-                        +function(a, p, P, b, y) {
-                            appboy = {};
-                            appboyQueue = [];
-                            for (var s = "initialize destroy getDeviceId toggleAppboyLogging setLogger openSession changeUser requestImmediateDataFlush requestFeedRefresh subscribeToFeedUpdates logCardImpressions logCardClick logFeedDisplayed requestInAppMessageRefresh logInAppMessageImpression logInAppMessageClick logInAppMessageButtonClick logInAppMessageHtmlClick subscribeToNewInAppMessages removeSubscription removeAllSubscriptions logCustomEvent logPurchase isPushSupported isPushBlocked isPushGranted isPushPermissionGranted registerAppboyPushMessages unregisterAppboyPushMessages submitFeedback trackLocation stopWebTracking resumeWebTracking ab ab.User ab.User.Genders ab.User.NotificationSubscriptionTypes ab.User.prototype.getUserId ab.User.prototype.setFirstName ab.User.prototype.setLastName ab.User.prototype.setEmail ab.User.prototype.setGender ab.User.prototype.setDateOfBirth ab.User.prototype.setCountry ab.User.prototype.setHomeCity ab.User.prototype.setLanguage ab.User.prototype.setEmailNotificationSubscriptionType ab.User.prototype.setPushNotificationSubscriptionType ab.User.prototype.setPhoneNumber ab.User.prototype.setAvatarImageUrl ab.User.prototype.setLastKnownLocation ab.User.prototype.setUserAttribute ab.User.prototype.setCustomUserAttribute ab.User.prototype.addToCustomAttributeArray ab.User.prototype.removeFromCustomAttributeArray ab.User.prototype.incrementCustomUserAttribute ab.User.prototype.addAlias ab.InAppMessage ab.InAppMessage.SlideFrom ab.InAppMessage.ClickAction ab.InAppMessage.DismissType ab.InAppMessage.OpenTarget ab.InAppMessage.ImageStyle ab.InAppMessage.TextAlignment ab.InAppMessage.Orientation ab.InAppMessage.CropType ab.InAppMessage.prototype.subscribeToClickedEvent ab.InAppMessage.prototype.subscribeToDismissedEvent ab.InAppMessage.prototype.removeSubscription ab.InAppMessage.prototype.removeAllSubscriptions ab.InAppMessage.Button ab.InAppMessage.Button.prototype.subscribeToClickedEvent ab.InAppMessage.Button.prototype.removeSubscription ab.InAppMessage.Button.prototype.removeAllSubscriptions ab.SlideUpMessage ab.ModalMessage ab.FullScreenMessage ab.HtmlMessage ab.ControlMessage ab.Feed ab.Feed.prototype.getUnreadCardCount ab.Card ab.ClassicCard ab.CaptionedImage ab.Banner ab.WindowUtils display display.automaticallyShowNewInAppMessages display.showInAppMessage display.showFeed display.destroyFeed display.toggleFeed sharedLib".split(" "), i = 0; i < s.length; i++) {
-                                for (var m = s[i], k = appboy, l = m.split("."), j = 0; j < l.length - 1; j++) k = k[l[j]];
-                                k[l[j]] = (new Function("return function " + m.replace(/\./g, "_") + "(){appboyQueue.push(arguments)}"))()
-                            }
-                            appboy.getUser = function() {
-                                return new appboy.ab.User
-                            };
-                            appboy.getCachedFeed = function() {
-                                return new appboy.ab.Feed
-                            };
+                    appboy.initialize(forwarderSettings.apiKey, options);
 
-                            (y = p.createElement(P)).type = 'text/javascript';
-                            y.src = 'https://js.appboycdn.com/web-sdk/2.0/appboy.min.js';
+                    if (forwarderSettings.register_inapp == 'True') {
+                        appboy.display.automaticallyShowNewInAppMessages();
+                    }
 
-                            y.async = 1;
-                            (b = p.getElementsByTagName(P)[0]).parentNode.insertBefore(y, b)
-                        }(window, document, 'script');
-
-                        appboy.initialize(forwarderSettings.apiKey, options);
-
-                        if (forwarderSettings.register_inapp == 'True') {
-                            appboy.display.automaticallyShowNewInAppMessages();
-                        }
-
-                        appboy.openSession();
-                        appboy.requestInAppMessageRefresh();
-                    }();
-                    /* eslint-enable */
+                    appboy.openSession();
+                    appboy.requestInAppMessageRefresh();
                 }
                 else {
                     if (!(appboy.initialize(forwarderSettings.apiKey, options))) {
