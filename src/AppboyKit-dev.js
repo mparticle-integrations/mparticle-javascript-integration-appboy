ecb5724c35b6563edc2dac1e7c0e7a90d9f094fe
Robert Ing <ring@mparticle.com>
2021-07-08 09:23:16 -0400

    fix: check for current Braze instance

--- src/AppboyKit-dev.js
+++ src/AppboyKit-dev.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
-window.appboy = require('appboy-web-sdk');
+if (!window.appboy) {
+    window.appboy = require('appboy-web-sdk');
+}
 //  Copyright 2015 mParticle, Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
